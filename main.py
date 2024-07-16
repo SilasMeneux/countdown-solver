7f5d6cf1c24aa384175bc0ff20d2fb33c703cdba
45LukeSkywalker <153367315+45LukeSkywalker@users.noreply.github.com>
2024-07-16 19:25:06 +0100

    fixed thingy being buggy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-while ans not in [1, 2, 3]:
-    ans = int(input("""Choose an option:
+answer = 0
+while answer not in [1, 2, 3]:
+    answer = int(input("""Choose an option:
     1. Words
     2. Numbers
     3. Conundrum"""))
