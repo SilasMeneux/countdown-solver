ace93ed0834b20bcdb84bf3f3fd9995236b538cf
Silas Meneux <silasmeneux@gmail.com>
2024-07-16 19:25:44 +0100

    worked out main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-while ans not in [1, 2, 3]:
-    ans = int(input("""Choose an option:
-    1. Words
-    2. Numbers
-    3. Conundrum"""))
+#while ans not in [1, 2, 3]:
+    #ans = int(input("""Choose an option:
+    #1. Words
+    #2. Numbers
+    #3. Conundrum"""))
+
+import solver
+
